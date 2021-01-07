27c9387da67f3ab5f494ede004d64aa26a586268
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-01-08 01:58:10 +0530

    sm6150: Disable DLKM
    Change-Id: I42fd414e3caece77b701281e4ec141599f8122f7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -147,6 +147,9 @@ TARGET_USES_NEW_ION_API := true
 TARGET_USES_QCOM_BSP := false
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 androidboot.usbcontroller=a600000.dwc3 earlycon=msm_geni_serial,0x880000 loop.max_part=7 androidboot.selinux=permissive
 
+# Disable DLKM
+TARGET_KERNEL_DLKM_DISABLE := true
+
 BOARD_EGL_CFG := device/qcom/$(TARGET_BOARD_PLATFORM)/egl.cfg
 
 BOARD_KERNEL_BASE        := 0x00000000
